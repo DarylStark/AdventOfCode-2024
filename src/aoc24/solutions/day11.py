c541566358c694be137e9155909c9a4496e82c1b
Daryl Stark <daryl@dstark.nl>
2024-12-11 09:47:52 +0000

    feat: Add code for Day 11 - Puzzle 2

--- src/aoc24/solutions/day11.py
+++ src/aoc24/solutions/day11.py
@@ -20,6 +20,7 @@ class Day11(DaySolution):
         self._loaded_data = False
         self._stones: list[int] = []
         self._blinks = 0
+        self._cache: dict[tuple[int, int], int] = {}
 
     def _load_data(self) -> None:
         """Load data from the input file."""
@@ -29,46 +30,55 @@ class Day11(DaySolution):
         with open(self._input_file, encoding='utf-8') as file:
             self._stones = [int(stone) for stone in file.read().split()]
 
-    def _blink(self) -> None:
-        """Blink one time.
+    def _stone_count(self, value: int, increment: int) -> int:
+        """Blink and return the new count of the number.
 
-        Will execute the required rules on the stonelist.
+        Args:
+            value: the value for the stone.
+            increment: the increment level we are on.
+
+        Returns:
+            The amount of stones you get from this specific value.
         """
-        stone_index = 0
-        self._blinks += 1
-        while True:
-            if stone_index >= len(self._stones):
-                break
+        if increment == 0:
+            return 1
 
-            if self._stones[stone_index] == 0:
-                # Stone is `0`, we replace it by a stoned with `1`
-                self._stones[stone_index] = 1
-                stone_index += 1
-                continue
+        if (value, increment) in self._cache:
+            return self._cache[(value, increment)]
 
-            if len(str(self._stones[stone_index])) % 2 == 0:
-                # Stone has a even "length"
-                stone_engraving = str(self._stones[stone_index])
-                stone1 = int(stone_engraving[0 : len(stone_engraving) // 2])
-                stone2 = int(stone_engraving[len(stone_engraving) // 2 :])
-                self._stones[stone_index] = stone1
-                self._stones.insert(stone_index + 1, stone2)
-                stone_index += 2
-                continue
+        if value == 0:
+            new_value = self._stone_count(1, increment - 1)
 
-            self._stones[stone_index] *= 2024
-            stone_index += 1
+        elif len(str(value)) % 2 == 0:
+            value_as_string = str(value)
+            stone1 = int(value_as_string[0 : len(value_as_string) // 2])
+            stone2 = int(value_as_string[len(value_as_string) // 2 :])
+            new_value = self._stone_count(
+                stone1, increment - 1
+            ) + self._stone_count(stone2, increment - 1)
+        else:
+            new_value = self._stone_count(value * 2024, increment - 1)
+
+        self._cache[(value, increment)] = new_value
+
+        return new_value
 
     def solve_puzzle_one(self) -> str:
         """Solve puzzle one."""
         self._load_data()
 
-        for _ in range(0, 25):
-            self._blink()
+        count = 0
+        for stone in self._stones:
+            count += self._stone_count(stone, 25)
 
-        return str(len(self._stones))
+        return str(count)
 
     def solve_puzzle_two(self) -> str:
         """Solve puzzle two."""
         self._load_data()
-        return ''
+
+        count = 0
+        for stone in self._stones:
+            count += self._stone_count(stone, 75)
+
+        return str(count)
